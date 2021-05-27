e41f2838452457e9fc918ff96af9a85924c9053e
nicsum <danbildan@gmail.com>
2021-05-27 12:48:00 +0300

    Initial commit

---
+++ app/src/main/java/com/danbi/handlerhttp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.danbi.handlerhttp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
